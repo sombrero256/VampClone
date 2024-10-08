47a8c69543bab6275be4bfe335f7069f50a273ea
Sombo <michael.rohrssen@gmail.com>
2024-10-08 12:56:49 -0500

    initial commit

---
+++ movement_component.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+var parent: Node2D
+var player_stats: Stats
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	parent = get_parent()
+	if parent.has_node('Stats'):
+		player_stats = parent.find_child('Stats')
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	var dir = Vector2()
+	if Input.is_action_pressed("down"):
+		dir.y += 1
+	if Input.is_action_pressed("up"):
+		dir.y -= 1
+	if Input.is_action_pressed("right"):
+		dir.x += 1
+	if Input.is_action_pressed("left"):
+		dir.x -= 1
+	# normalize direction
+	dir = dir.normalized()
+	
+	if parent is RigidBody2D:
+		parent.move_and_collide(dir * player_stats.Speed)
---
+++ stats_component.gd
@@ -0,0 +1,7 @@
+class_name Stats extends Node2D
+
+@export
+var Speed = 3
+@export
+var Max_Health = 100
+var Cur_Health = Max_Health
