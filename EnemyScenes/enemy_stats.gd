2c51e87ae4742420d6491c6aaec3d9ce8658f13f
sombrero256 <michael.rohrssen@gmail.com>
2024-10-13 14:40:13 -0500

    added functionality to deal damage to player via signals

--- EnemyScenes/enemy_stats.gd
+++ EnemyScenes/enemy_stats.gd
@@ -1,7 +1,9 @@
-class_name PlayerStats extends Node2D
+class_name EnemyStats extends Node
 
 @export
 var Speed = 3
 @export
 var Max_Health = 100
+@export
+var DPS = 7
 var Cur_Health = Max_Health
--- PlayerScenes/movement_component.gd
+++ PlayerScenes/movement_component.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var parent: Node2D
-var player_stats: Stats
+var player_stats: PlayerStats
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	parent = get_parent()
