740209e0b1874cf024dcbaa26d4775dba4c2a53b
sombo <michael.rohrssen@gmail.com>
2024-10-13 22:59:23 -0500

    changed enemies into rigid bodies so that the player can push them around

--- EnemyScenes/enemy_movement_component.gd
+++ EnemyScenes/enemy_movement_component.gd
@@ -1,18 +1,20 @@
 extends Node2D
 
-@onready var parent = get_parent() as CharacterBody2D
+@onready var parent = get_parent() as RigidBody2D
 @onready var player = get_tree().get_first_node_in_group("Player")
 # Called when the node enters the scene tree for the first time.
+
+var velocity
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var dir = parent.position.direction_to(player.position)
-	parent.velocity = dir * 50
 	pass
 
 func _physics_process(delta: float) -> void:
-	parent.move_and_slide()
+	var dir = parent.position.direction_to(player.position)
+	velocity = dir * 50
+	parent.move_and_collide(velocity * delta)
 	pass
