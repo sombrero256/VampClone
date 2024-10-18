a89d66a248c8212c41104085cf8635c9a6ff04c6
sombo <michael.rohrssen@gmail.com>
2024-10-17 20:40:46 -0500

    added healthbar ui

---
+++ UI/HealthBar/health_bar_display.gd
@@ -0,0 +1,16 @@
+extends ColorRect
+
+var stats: PlayerStats
+@onready var starting_size = size.x
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var player = get_tree().get_first_node_in_group('Player')
+	stats = player.find_child('Stats')
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print(stats.Cur_Health)
+	size.x = starting_size * (stats.Cur_Health / stats.Max_Health) 
+	pass
