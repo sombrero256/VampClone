174430183fd08afcf0e56ec4b8ce4c6c6f05a7c7
sombo <michael.rohrssen@gmail.com>
2024-10-19 13:55:23 -0500

    added death particle and health gradient

--- EnemyScenes/enemy.gd
+++ EnemyScenes/enemy.gd
@@ -3,12 +3,16 @@ extends CharacterBody2D
 const MAX_HEALTH = 300
 @onready var rect = get_node("ColorRect") as ColorRect
 @onready var health = get_node("ProgressBar") as ProgressBar
+@onready var death_particle = preload("res://EnemyScenes/DeathParticle/EnemyDeathParticle.tscn")
 
 # Eventually this returns rewards if killed
 func process_hit(damages) -> void:
 	health.value -= damages
 	rect.color = "a356ff"
 	if health.value == 0:
+		var _inst = death_particle.instantiate() as Node2D
+		_inst.global_position = position
+		get_node("/root").add_child(_inst)
 		queue_free()
 	#print(health)
 
