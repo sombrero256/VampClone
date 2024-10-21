075c1b0732452b8ac799ec28bb00d5ec32559307
Jeorin <orion_christ@hotmail.com>
2024-10-20 17:52:30 -0700

    Simple Mob Spawner

---
+++ test.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+##Creates new enemy object along the MonsterSpawnPath outside the camera position
+##If we make new enemy types, we'll have to duplicate this for each type, or randomize it
+func spawn_enemy():
+	var new_enemy = preload("res://EnemyScenes/enemy.tscn").instantiate()
+	%MonsterSpawnPath.progress_ratio = randf()
+	new_enemy.global_position = %MonsterSpawnPath.global_position
+	add_child(new_enemy)
+	new_enemy.add_to_group("Enemy")
+	
+##Spawns enemies on timer.  Can adjust how many enemies.  Might be best to change to a loop at some point.
+func _on_timer_timeout():
+	spawn_enemy()
+	spawn_enemy()
+	spawn_enemy()
+	spawn_enemy()
+	spawn_enemy()
