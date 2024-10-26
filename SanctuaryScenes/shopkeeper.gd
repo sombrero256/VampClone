f46f8503f98cb1ca5c1d28f675e53919ed1dd80c
Jeorin <orion_christ@hotmail.com>
2024-10-25 20:44:31 -0700

    Update that recognizes the player is near
    Player detection working, but we need the dialogue node to fire, which I can't seem to do.

--- SanctuaryScenes/shopkeeper.gd
+++ SanctuaryScenes/shopkeeper.gd
@@ -2,16 +2,17 @@ extends CharacterBody2D
 
 var player_chatting = false
 
-func _ready():	
+func _process(delta):	
 	##tried to make chatting work and I can't.  Tutorial for it was bad.
 	if Input.is_action_just_pressed("chat")  && player_chatting == true:
 		print("chatting is working")
-		$Dialogue/Dialogue.start()
+		##need something to access the start function in the dialogue node here
 
 
 func _on_chat_zone_body_entered(body):
-		print("guy is near")
-		player_chatting = true
+		if body.name == "JustAGuy":
+			print("guy is near")
+			player_chatting = true
 
 
 func _on_chat_zone_body_exited(body):
