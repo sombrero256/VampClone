b48f7b9b2ab36cb5a640e610874b778b629090b1
Jeorin <orion_christ@hotmail.com>
2024-10-26 12:08:00 -0700

    Working dialogue!
    Putting this here first before I try to merge with Master.

--- SanctuaryScenes/dialogue.gd
+++ SanctuaryScenes/dialogue.gd
@@ -4,23 +4,30 @@ extends Control
 @export_file("*.json") var dialogue_file
 
 var dialogue = []
-var current_dialogue_id = -1
+var current_dialogue_id = 0
 var dialogue_active = false
 
 func _ready():
 	$ColorRect.visible = false
 
-func start():
-	if dialogue_active:
-		return
-	dialogue_active = true
-	$ColorRect.visible = true
-	dialogue = load_dialogue()
-	current_dialogue_id = -1
-	next_script()
+func start(speaker):
+	if !dialogue_active:
+		dialogue_active = true
+		$ColorRect.visible = true
+		dialogue = load_dialogue(speaker)
+		current_dialogue_id = -1
+		next_script()
 	
-func load_dialogue():
-	var file = FileAccess.open("res://SanctuaryScenes/Dialogue/Shopkeeper1_dialogue.json", FileAccess.READ)
+func load_dialogue(speaker_select):
+	var file
+	if speaker_select == "MrDog":
+		print ("talking to dog")
+		file = FileAccess.open("res://SanctuaryScenes/MrDog_dialogue.json", FileAccess.READ)
+	elif speaker_select == "MrsCat":
+		print ("talking to cat")
+		file = FileAccess.open("res://SanctuaryScenes/MrsCat_dialogue.json", FileAccess.READ)
+	else:
+		file = FileAccess.open("res://SanctuaryScenes/default_dialogue.json", FileAccess.READ)
 	var content = JSON.parse_string(file.get_as_text())
 	return content
 	
@@ -36,7 +43,7 @@ func next_script():
 		dialogue_active = false
 		$ColorRect.visible = false
 		return
-		
-	$ColorRect/Name.text = dialogue[current_dialogue_id]['name']
-	$ColorRect/Text.text = dialogue[current_dialogue_id]['text']
+	else:
+		$ColorRect/SpeakerName.text = dialogue[current_dialogue_id]['name']
+		$ColorRect/Text.text = dialogue[current_dialogue_id]['text']
 	
--- SanctuaryScenes/shopkeeper.gd
+++ SanctuaryScenes/shopkeeper.gd
@@ -5,8 +5,11 @@ var player_chatting = false
 func _process(delta):	
 	##tried to make chatting work and I can't.  Tutorial for it was bad.
 	if Input.is_action_just_pressed("chat")  && player_chatting == true:
-		print("chatting is working")
+		print("chatting is working with")
+		print(self.name)
 		##need something to access the start function in the dialogue node here
+		player_chatting = false
+		$Dialogue.start(self.name)
 
 
 func _on_chat_zone_body_entered(body):
