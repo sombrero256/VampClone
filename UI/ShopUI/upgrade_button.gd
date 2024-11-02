4055a694c8e4b1719c9c60f55cb55c4c43e35a4f
sombrero256 <michael.rohrssen@gmail.com>
2024-11-02 10:35:23 -0500

    button now has costs and scaling

--- UI/ShopUI/upgrade_button.gd
+++ UI/ShopUI/upgrade_button.gd
@@ -1,7 +1,25 @@
 extends TextureRect
 
 @export var CorrespondingWeapon: BaseWeapon.WeaponType
+
+@export var AreaLvlUpAmt: float
+@export var SpeedLvlUpAmt: float
+@export var DamageLvlUpAmt: float
+
+@export var SpeedCostScaling: float
+@export var AreaCostScaling: float
+@export var DamageCostScaling: float
+
+@export var SpeedBaseCost : int
+@export var AreaBaseCost: int
+@export var DamageBaseCost: int
+
+@export var SpeedUpgradeAnimal : Enemy.EnemyType
+@export var AreaUpgradeAnimal : Enemy.EnemyType
+@export var DamageUpgradeAnimal : Enemy.EnemyType
+
 func _ready() -> void:
+	update_weapon_icon()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -9,25 +27,29 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_area_upgrade_button_button_up() -> void:
-	Globalstats._weapon_stats[CorrespondingWeapon].area_ += .1
-	Globalstats._weapon_stats[CorrespondingWeapon].area_levels += 1
-	update_labels()
+	if subtract_upgrade_cost(Globalstats._weapon_stats[CorrespondingWeapon].area_levels,  AreaCostScaling, AreaBaseCost, AreaUpgradeAnimal):
+		Globalstats._weapon_stats[CorrespondingWeapon].lvl_up(1, 1, AreaLvlUpAmt, [])
+		Globalstats._weapon_stats[CorrespondingWeapon].area_levels += 1
+		update_labels()
 	pass # Replace with function body.
 
 func _on_damage_upgrade_button_button_up() -> void:
-	Globalstats._weapon_stats[CorrespondingWeapon].damage_ += 1
-	Globalstats._weapon_stats[CorrespondingWeapon].damage_levels += 1
-	update_labels()
+	if subtract_upgrade_cost(Globalstats._weapon_stats[CorrespondingWeapon].damage_levels,  DamageCostScaling, DamageBaseCost, DamageUpgradeAnimal):
+		Globalstats._weapon_stats[CorrespondingWeapon].lvl_up(DamageLvlUpAmt, 1, 1, [])
+		Globalstats._weapon_stats[CorrespondingWeapon].damage_levels += 1
+		update_labels()
 	pass # Replace with function body.
 
 func _on_speed_upgrade_button_button_up() -> void:
-	Globalstats._weapon_stats[CorrespondingWeapon].speed_ += 1
-	Globalstats._weapon_stats[CorrespondingWeapon].speed_levels += 1
-	update_labels()
+	if subtract_upgrade_cost(Globalstats._weapon_stats[CorrespondingWeapon].speed_levels,  SpeedCostScaling, SpeedBaseCost, SpeedUpgradeAnimal):
+		Globalstats._weapon_stats[CorrespondingWeapon].lvl_up(1, SpeedLvlUpAmt, 1, [])
+		Globalstats._weapon_stats[CorrespondingWeapon].speed_levels += 1
+		update_labels()
 	pass # Replace with function body.
 
 func _on_visibility_changed() -> void:
 	update_labels()
+	
 	pass # Replace with function body.
 
 func update_labels() -> void:
@@ -35,3 +57,17 @@ func update_labels() -> void:
 	%DamageUpgradeLabel.text = str(Globalstats._weapon_stats[CorrespondingWeapon].damage_levels)
 	%SpeedUpgradeLabel.text = str(Globalstats._weapon_stats[CorrespondingWeapon].speed_levels)
 	pass
+
+func update_weapon_icon() -> void:
+	var weapon_icon_dict = {}
+	weapon_icon_dict[BaseWeapon.WeaponType.HEART] = "res://WeaponScenes/Heart/heart.png"
+	weapon_icon_dict[BaseWeapon.WeaponType.BOOMERANG] = "res://WeaponScenes/Boomerang/boom2.png"
+	weapon_icon_dict[BaseWeapon.WeaponType.BUBBLE] = "res://WeaponScenes/StarBomb/assets/Bubble 1.png"
+	%WeaponIcon.texture = load(weapon_icon_dict[CorrespondingWeapon])
+	pass
+
+func subtract_upgrade_cost(levels: int, scale_factor: float, base_cost: int, animal_used: Enemy.EnemyType) -> bool:
+	if Globalstats.GetSavedEnemies()[animal_used] >= int(base_cost + (levels * scale_factor)):
+		Globalstats._enemy_saved[animal_used] -= int(base_cost + (levels * scale_factor))
+		return true
+	return false
